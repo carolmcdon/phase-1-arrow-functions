914473765593af2138e44f749ab345217722885f
Caroline <ckmcdon@gmail.com>
2022-05-16 21:33:49 -0400

    commit tree 5fc1cb300db88771b4606a0aea10506bd8f44686

---
+++ index.js
@@ -0,0 +1,14 @@
+const divide = function(x, y) {
+    return (x)/(y)
+}
+divide(2000)(100);
+
+
+function square(x) {
+    return (x ** 2);
+}
+
+function add(x, y) {
+    return x + y;
+}
+add(3,4);
